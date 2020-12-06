98fff4298d95ad0e9da01f2f87db042b21db6cf4
Mayur Ravji <mayur.ravji29@gmail.com>
2020-12-06 13:47:06 -0500

    Added database functionality

---
+++ app/src/main/java/project/st991438136/alex/database/DBModel.kt
@@ -0,0 +1,153 @@
+package project.st991438136.alex.database
+
+import android.util.Log
+import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+object DBModel: Observable() {
+
+    /* declare a couple constants
+     * TAG : log tag
+     * WEIGHTS : link to weights table in firebase
+     * RUNNING : link to running table in firebase
+     */
+    private const val TAG = "WorkoutData"
+    private const val WEIGHTS = "https://workoutfinalproject-default-rtdb.firebaseio.com/weights"
+    private const val RUNNING = "https://workoutfinalproject-default-rtdb.firebaseio.com/running"
+
+    // create event listeners
+    private var eventListener_weights : ValueEventListener? = null
+    private var eventListener_running : ValueEventListener? = null
+
+    // prepare arraylists to store data
+    private var listWeights : ArrayList<Weights>? = ArrayList()
+    private var listRunning : ArrayList<Running>? = ArrayList()
+
+    // link to each table
+    private fun dbRefModelWeights() : DatabaseReference? =
+        FirebaseDatabase.getInstance().reference.child(WEIGHTS)
+
+    private fun dbRefModelRunning() : DatabaseReference? =
+        FirebaseDatabase.getInstance().reference.child(RUNNING)
+
+    // set eventlisteners to null
+    private fun setAllNulls() {
+
+        eventListener_running = null
+        eventListener_weights = null
+    }
+
+    // remove existing event listeners
+    private fun removeListener(listener : ValueEventListener?) {
+
+        if (listener != null) {
+            when (listener) {
+                eventListener_weights ->
+                    dbRefModelWeights()?.removeEventListener(listener)
+                eventListener_running ->
+                    dbRefModelRunning()?.removeEventListener(listener)
+            }
+        }
+    }
+
+
+    // catch method
+    private fun catchMethod(e : Exception, type : String) {
+        Log.i("$TAG: $type", "Error: \n ${e.message}" )
+    }
+
+    // update method
+    private fun update(type : String) {
+        Log.i("$TAG: $type", "Data updated, there are +" +
+                "${listRunning!!.size} entries in the cache" )
+        setChanged()
+        notifyObservers()
+    }
+
+    // getter methods
+
+    fun getRunningData() : ArrayList<Running>? = listRunning
+    fun getWeightsData() : ArrayList<Weights>? = listWeights
+
+    // init
+    init {
+
+        removeListener(eventListener_running)
+        removeListener(eventListener_weights)
+        setAllNulls()
+
+        // adding event listeners to running
+        eventListener_running = object : ValueEventListener {
+
+            // occurs when data is changed in the database
+            override fun onDataChange(snapshot : DataSnapshot) {
+                try {
+                    val data : ArrayList<Running> = ArrayList()
+                    for (dataSnapshot : DataSnapshot in  snapshot.children) {
+
+                        try {
+
+                            data.add(Running(dataSnapshot))
+
+                        } catch (e: Exception) {
+                            catchMethod(e,"Running")
+                        }
+                    }
+
+                    listRunning = data
+                    update("Running")
+
+
+                } catch (e: Exception) {
+                    catchMethod(e,"Running")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("$TAG: Running", "Error: \n${error.message}")
+            }
+
+
+        }
+
+        eventListener_weights = object : ValueEventListener {
+
+            // occurs when data is changed in the database
+            override fun onDataChange(snapshot : DataSnapshot) {
+                try {
+                    val data : ArrayList<Weights> = ArrayList()
+                    for (dataSnapshot: DataSnapshot in  snapshot.children) {
+
+                        try {
+
+                            data.add(Weights(dataSnapshot))
+
+                        } catch (e: Exception) {
+                            catchMethod(e,"Weights")
+                        }
+                    }
+
+                    listWeights = data
+                    update("Weights")
+
+
+                } catch (e: Exception) {
+                    catchMethod(e,"Weights")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("$TAG: Weights", "Error: \n${error.message}")
+            }
+
+
+
+        }
+
+
+        dbRefModelRunning()?.addValueEventListener(eventListener_running!!)
+        dbRefModelWeights()?.addValueEventListener(eventListener_weights!!)
+    }
+
+}
---
+++ app/src/main/java/project/st991438136/alex/database/Weights.kt
@@ -0,0 +1,27 @@
+package project.st991438136.alex.database
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+
+class Weights (snapshot: DataSnapshot) {
+
+    lateinit var date : String
+    lateinit var duration : String
+    lateinit var weightsUsed : String
+    lateinit var repsComplete : String
+
+    init {
+        try {
+            val data : HashMap<String, Any> = snapshot.value as HashMap<String, Any>
+
+            date = snapshot.key ?: ""
+            duration = data["duration"] as String
+            weightsUsed = data["weightsUsed"] as String
+            repsComplete = data["repsComplete"] as String
+
+        } catch (e: Exception) {
+
+            Log.i("WeightsDB", "An error has occurred: \n $e")
+        }
+    }
+}
